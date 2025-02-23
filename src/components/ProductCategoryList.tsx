1ff7b84d710d73eb1c7f0de76472696cda70e4fa
Demis Getachew <demisgech@gmail.com>
2025-02-23 14:45:11 +0300

    Refactor: remove unnecessary code.

--- src/components/ProductCategoryList.tsx
+++ src/components/ProductCategoryList.tsx
@@ -1,11 +1,7 @@
 import useProductCategoryList from "../hooks/useProductCategoryList";
-import Spinner from "./Spinner";
 
 const ProductCategoryList = () => {
-  const { categories, error, isLoading } = useProductCategoryList();
-
-  if (isLoading) return <Spinner isLoading={isLoading} />;
-  if (error) return <div className="text-red-400">{error}</div>;
+  const { categories } = useProductCategoryList();
 
   return (
     <select className="block w-[100%] p-1 border-1 border-gray-500 rounded-md text-gray-700 outline-0 text-2xl">
@@ -15,7 +11,6 @@ const ProductCategoryList = () => {
       {categories &&
         categories.map((category) => (
           <option className="text-green-700 outline-0" value={category}>
-            {" "}
             {category}
           </option>
         ))}
