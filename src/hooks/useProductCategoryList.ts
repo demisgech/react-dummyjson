4601c42f24a48baa5712a596a1e81478525d080b
Demis Getachew <demisgech@gmail.com>
2025-02-23 14:39:15 +0300

    Ship category list data with the app

--- src/hooks/useProductCategoryList.ts
+++ src/hooks/useProductCategoryList.ts
@@ -1,38 +1,48 @@
-import { CanceledError, AxiosError } from "axios";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
+// import { CanceledError, AxiosError } from "axios";
+// import { useState, useEffect } from "react";
+// import apiClient from "../services/api-client";
+import { getProductCategoryList } from "../data/getProductCategoryList";
 
-interface FetchCategoryResponse {
-  (): string[];
-}
+// First Option
+const useProductCategoryList = () => ({
+  categories: getProductCategoryList(),
+  error: null,
+  isLoading: false,
+});
 
-const useProductCategoryList = () => {
-  const [categories, setCategories] = useState<string[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+// Second option
+// interface FetchCategoryResponse {
+//   (): string[];
+// }
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchCategoryResponse>("/products/category-list", {
-        signal: controller.signal,
-      })
-      .then((respose) => {
-        console.log(respose.data);
-        setLoading(false);
-        setCategories(respose.data);
-      })
-      .catch((error) => {
-        // console.log(error);
-        if (error instanceof CanceledError) return;
-        setError((error as AxiosError).message);
-      });
+// const useProductCategoryList = () => {
+//   const [categories, setCategories] = useState<string[]>([]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false);
 
-    return () => controller.abort();
-  }, []);
+//   useEffect(() => {
+//     const controller = new AbortController();
+//     setLoading(true);
+//     apiClient
+//       .get<FetchCategoryResponse>("/products/category-list", {
+//         signal: controller.signal,
+//       })
+//       .then((respose) => {
+//         // console.log(respose.data);
+//         setLoading(false);
+//         setCategories(respose.data);
+//       })
+//       .catch((error) => {
+//         // console.log(error);
+//         if (error instanceof CanceledError) return;
+//         setLoading(false);
+//         setError((error as AxiosError).message);
+//       });
 
-  return { categories, error, isLoading };
-};
+//     return () => controller.abort();
+//   }, []);
+
+//   return { categories, error, isLoading };
+// };
 
 export default useProductCategoryList;
